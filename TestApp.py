9ffa2145e661a0598ea30c376f864936e58b4ad2
Lucas Sernajoto Vanzeler Paixao <lucas.44822@alunosatc.edu.br>
2025-09-05 20:40:18 -0300

    fix: teste

--- TestApp.py
+++ TestApp.py
@@ -6,6 +6,6 @@ def test_soma():
     assert soma(0, 0) == 0
 
 def test_multiplicacao():
-    assert soma(1, 1) == 1
-    assert soma(10, 10) == 100
-    assert soma(0, 15) == 0
+    assert multiplicacao(1, 1) == 1
+    assert multiplicacao(10, 10) == 100
+    assert multiplicacao(0, 15) == 0
